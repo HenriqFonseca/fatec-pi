5e1b7f9cabba491ee89558efdd25f65abe4f63eb
Gabriel Meira <gabriel_meira10@hotmail.com>
2023-05-26 15:23:36 -0300

    Nova navbar legal :+1:

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,3 +1,5 @@
+/* Responsive menu */
+
 const menuToggleButton = document.querySelector('#menu-toggle');
 const menuToggleIcon = document.querySelector('#menu-toggle-icon')
 const linksWrapper = document.querySelector('#links-wrapper');
@@ -5,4 +7,15 @@ const linksWrapper = document.querySelector('#links-wrapper');
 menuToggleButton.addEventListener('click', () => {
     linksWrapper.classList.toggle('menu-open');
     menuToggleIcon.innerText = menuToggleIcon.innerText == 'menu' ? 'close' : 'menu';
+})
+
+/* Change background color when navbar is out of position */
+
+const navbarElement = document.querySelector('.navbar');
+
+document.addEventListener('scroll', (e) => {
+    
+    let top = (window.pageYOffset || document.scrollTop)  - (document.clientTop || 0);
+
+    navbarElement.style.background = top > 120 ? 'rgba(0, 0, 0, 1)' : 'rgba(0, 0, 0, 0)';
 })
