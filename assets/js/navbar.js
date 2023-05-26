6c17fe2ae029459657a6a29bb2e93d8d4bd8a5d9
Gabriel Meira <gabriel_meira10@hotmail.com>
2023-05-26 16:28:06 -0300

    Criando o arquivo de testes e arrumando detalhes

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -17,5 +17,6 @@ document.addEventListener('scroll', (e) => {
     
     let top = (window.pageYOffset || document.scrollTop)  - (document.clientTop || 0);
 
-    navbarElement.style.background = top > 120 ? 'rgba(0, 0, 0, 1)' : 'rgba(0, 0, 0, 0)';
+    navbarElement.style.background = top > 120 ? 'rgba(0, 0, 0, .5)' : 'rgba(0, 0, 0, 0)';
+    navbarElement.style.backdropFilter = top > 120 ? 'blur(20px)' : 'blur(0)';
 })
