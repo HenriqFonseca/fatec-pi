ddd9880ea37945849b370f715887357dfcde5d20
Henriq_Fonseca <henrique12095@gmail.com>
2023-06-06 20:00:35 -0300

    test

--- php/contatoForm.php
+++ php/contatoForm.php
@@ -16,17 +16,20 @@
       <p><b>Mensagem: </b>$mensagem</p>
       <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
     </html>
-  ";
-  
+    ";
+    
+    
+    echo "arquivo: $arquivo";
+
   //Emails para quem será enviado o formulário
   $destino = "henrique12095@gmail.com";
   $assunto = "Contato pelo Site";
-
+  
   //Este sempre deverá existir para garantir a exibição correta dos caracteres
   $headers  = "MIME-Version: 1.0\n";
   $headers .= "Content-type: text/html; charset=iso-8859-1\n";
   $headers .= "From: $nome <$email>";
-
+  
   //Enviar
   $email = mail($destino, $assunto, $arquivo);
   
