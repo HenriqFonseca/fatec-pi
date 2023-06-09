c5095d6a22f90b3b590206709f009cf6a00af4aa
Henriq_Fonseca <henrique12095@gmail.com>
2023-06-09 20:37:08 -0300

    links  footer

--- php/conection.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    define('HOST', 'localhost');
-    define('USER', 'root');
-    define('PASSWORD', '');
-    define('DBNAME', 'pi');
-
-    $conection = new mysqli(HOST,USER,PASSWORD,DBNAME);
-
-?>
